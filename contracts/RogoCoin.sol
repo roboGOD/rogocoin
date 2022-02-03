d2e79110961b9d65e582b67fe31cdbcdbe016a0f
roboGOD <ishwer.kumar666@gmail.com>
2022-02-03 10:35:09 +0530

    delegated transfer

--- contracts/RogoCoin.sol
+++ contracts/RogoCoin.sol
@@ -12,23 +12,57 @@ contract RogoCoin {
 
   mapping(address => uint256) public balanceOf;
 
+  mapping(address => mapping (address => uint256)) public allowance;
+
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
   constructor(uint256 _initialSupply) public {
     totalSupply = _initialSupply;
     balanceOf[msg.sender] = _initialSupply;
   }
 
-  modifier hasBalance(uint256 value) {
-    require(balanceOf[msg.sender] >= value, "Balance should be greater than transaction amount.");
+  modifier hasBalance(address sender, uint256 value) {
+    require(balanceOf[sender] >= value, "Balance should be greater than transaction amount.");
     _;
   }
 
-  function transfer(address _to, uint256 _value) public hasBalance(_value) returns (bool success) {
+  modifier minimumAllowance(address owner, address spender, uint256 value) {
+    require(allowance[owner][spender] >= value, "Allowance should be greater than transaction amount.");
+    _;
+  }
+
+  function transfer(address _to, uint256 _value) public hasBalance(msg.sender, _value) returns (bool success) {
     address _from = msg.sender;
     balanceOf[_from] -= _value;
     balanceOf[_to] += _value;
 
+    emit Transfer(_from, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    address _owner = msg.sender;
+
+    allowance[_owner][_spender] = _value;
+
+    emit Approval(_owner, _spender, _value);
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public 
+    hasBalance(_from, _value) 
+    minimumAllowance(_from, msg.sender, _value) 
+    returns (bool success) {
+    address _spender = msg.sender;
+
+    // Perform the transer
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+
+    // Deduct the allowance
+    allowance[_from][_spender] -= _value;
     emit Transfer(_from, _to, _value);
     return true;
   }
--- test/RogoCoin.test.js
+++ test/RogoCoin.test.js
@@ -1,10 +1,14 @@
-const { assert } = require("chai");
+const chai = require("chai");
+const { assert, expect } = chai;
+chai.use(require("chai-as-promised"));
 
 const RogoCoin = artifacts.require("./RogoCoin.sol");
 
-const INIT_SUPPLY = 1000000;
-
 contract("RogoCoin", (accounts) => {
+  const INIT_SUPPLY = 1000000;
+  const ALLOWANCE = 2000;
+  const TRANSFER_AMOUNT = 250;
+
   before(async () => {
     this.rogoCoin = await RogoCoin.deployed();
   });
@@ -39,20 +43,6 @@ contract("RogoCoin", (accounts) => {
   });
 
   it("transfers token ownership", async () => {
-    const REVERT_MESSAGE =
-      "Transaction must be reverted if the message caller's account balance does not have enough tokens to spend.";
-    try {
-      const response = await this.rogoCoin.transfer.call(
-        accounts[1],
-        999999999999999,
-        { from: accounts[0] }
-      );
-      assert.fail(REVERT_MESSAGE);
-    } catch (error) {
-      assert(error.message.indexOf("revert") >= 0, REVERT_MESSAGE);
-    }
-
-    const TRANSFER_AMOUNT = 250;
     assert.equal(
       (await this.rogoCoin.balanceOf(accounts[1])).toNumber(),
       0,
@@ -69,22 +59,179 @@ contract("RogoCoin", (accounts) => {
     assert.equal(
       (await this.rogoCoin.balanceOf(accounts[0])).toNumber(),
       INIT_SUPPLY - TRANSFER_AMOUNT,
-      "balance of receiver after transaction should be " +
+      "balance of sender after transaction should be " +
         INIT_SUPPLY -
         TRANSFER_AMOUNT
     );
 
     assert.equal(receipt.logs.length, 1, "emits single event");
+    assert.equal(receipt.logs[0].event, "Transfer", "emits the Transfer event");
     const event = receipt.logs[0].args;
     assert.equal(accounts[0], event._from, "emits the correct sender address");
     assert.equal(accounts[1], event._to, "emits the correct receiver address");
     assert.equal(TRANSFER_AMOUNT, event._value, "emits the correct amount");
+  });
 
+  it("transfer returns boolean", async () => {
     const success = await this.rogoCoin.transfer.call(
       accounts[1],
       TRANSFER_AMOUNT,
       { from: accounts[0] }
     );
-    assert.equal(success, true, 'transfer must returns boolean');
+    assert.equal(success, true, "transfer must return boolean");
+  });
+
+  it("failed transfer transaction should be reverted", async () => {
+    const REVERT_MESSAGE =
+      "Transaction must be reverted if the message caller's account balance does not have enough tokens to spend.";
+    try {
+      const response = await this.rogoCoin.transfer.call(
+        accounts[1],
+        999999999999999,
+        { from: accounts[0] }
+      );
+      assert.fail(0, 1, REVERT_MESSAGE);
+    } catch (error) {
+      assert(error.message.indexOf("revert") >= 0, REVERT_MESSAGE);
+    }
+  });
+
+  it("initial allowance must be 0", async () => {
+    const allowance = await this.rogoCoin.allowance(accounts[0], accounts[1]);
+    assert.equal(allowance.toNumber(), 0, "initial allowance must be 0");
+  });
+
+  it("approves tokens for delegated transfer", async () => {
+    const success = await this.rogoCoin.approve.call(accounts[1], ALLOWANCE, {
+      from: accounts[0],
+    });
+    assert.equal(success, true, "approve must return true");
+  });
+
+  it("emits Approval event", async () => {
+    const receipt = await this.rogoCoin.approve(accounts[1], ALLOWANCE, {
+      from: accounts[0],
+    });
+    assert.equal(receipt.logs.length, 1, "emits single event");
+    assert.equal(
+      receipt.logs[0].event,
+      "Approval",
+      "must be the 'Approval' event"
+    );
+    const event = receipt.logs[0].args;
+    assert.equal(accounts[0], event._owner, "emits the correct owner address");
+    assert.equal(
+      accounts[1],
+      event._spender,
+      "emits the correct spender address"
+    );
+    assert.equal(ALLOWANCE, event._value, "emits the correct allowance");
+
+    const allowance = await this.rogoCoin.allowance(accounts[0], accounts[1]);
+    assert.equal(
+      allowance.toNumber(),
+      ALLOWANCE,
+      "allowance must be " + ALLOWANCE
+    );
+  });
+
+  it("handles the successful delegated transfer", async () => {
+    const fromAccount = accounts[2];
+    const toAccount = accounts[3];
+    const spenderAccount = accounts[4];
+
+    const initSupply = await this.rogoCoin.transfer(fromAccount, 10000, {
+      from: accounts[0],
+    });
+    const approved = await this.rogoCoin.approve(spenderAccount, ALLOWANCE, {
+      from: fromAccount,
+    });
+    const success = await this.rogoCoin.transferFrom.call(
+      fromAccount,
+      toAccount,
+      TRANSFER_AMOUNT,
+      { from: spenderAccount }
+    ); 
+    assert.equal(success, true, "transferFrom returns a boolean");
+
+    const delegatedTransfer = await this.rogoCoin.transferFrom(
+      fromAccount,
+      toAccount,
+      TRANSFER_AMOUNT,
+      { from: spenderAccount }
+    );
+
+    const receiverBalance = await this.rogoCoin.balanceOf(toAccount);
+    assert.equal(
+      receiverBalance.toNumber(),
+      TRANSFER_AMOUNT,
+      "Receiver balance must equal the transfer amount"
+    );
+    const ownerBalance = await this.rogoCoin.balanceOf(fromAccount);
+    assert.equal(
+      ownerBalance.toNumber(),
+      10000 - TRANSFER_AMOUNT,
+      "Owner balance must equal " + 10000 - TRANSFER_AMOUNT
+    );
+    const allowance = await this.rogoCoin.allowance(fromAccount, spenderAccount);
+    assert.equal(
+      allowance.toNumber(),
+      ALLOWANCE-TRANSFER_AMOUNT,
+      `allowance must be ${ALLOWANCE-TRANSFER_AMOUNT} after the transfer`
+    );
+  });
+  
+  it("transferFrom emits the Transfer event", async () => {
+    const fromAccount = accounts[2];
+    const toAccount = accounts[3];
+    const spenderAccount = accounts[4];
+
+    const initSupply = await this.rogoCoin.transfer(fromAccount, 10000, {
+      from: accounts[0],
+    });
+    const approved = await this.rogoCoin.approve(spenderAccount, ALLOWANCE, {
+      from: fromAccount,
+    });
+    const receipt = await this.rogoCoin.transferFrom(
+      fromAccount,
+      toAccount,
+      TRANSFER_AMOUNT,
+      { from: spenderAccount }
+    );
+
+    assert.equal(receipt.logs.length, 1, "emits single event");
+    assert.equal(receipt.logs[0].event, "Transfer", "emits the Transfer event");
+    const event = receipt.logs[0].args;
+    assert.equal(fromAccount, event._from, "emits the correct sender address");
+    assert.equal(toAccount, event._to, "emits the correct receiver address");
+    assert.equal(TRANSFER_AMOUNT, event._value, "emits the correct amount");
+  });
+
+  it("transferFrom throws error on failed transaction", async () => {
+    const fromAccount = accounts[2];
+    const toAccount = accounts[3];
+    const spenderAccount = accounts[4];
+
+    const initSupply = await this.rogoCoin.transfer(fromAccount, 10000, {
+      from: accounts[0],
+    });
+    const approved = await this.rogoCoin.approve(spenderAccount, ALLOWANCE, {
+      from: fromAccount,
+    });
+
+    const REVERT_MESSAGE = "Transaction must be reverted if allowance is less than transfer amount";
+    try {
+      await expect(
+        this.rogoCoin.transferFrom(
+          fromAccount,
+          toAccount,
+          2*ALLOWANCE,
+          { from: spenderAccount }
+        )
+      ).to.be.rejectedWith(Error);
+    } catch (error) {
+      console.log("ERROR IS: ", error.message);
+      assert(error.message.indexOf("revert") >= 0, REVERT_MESSAGE);
+    }
   });
 });
